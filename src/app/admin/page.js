b37c7717dc779f18113a05c6c64cecee2d592087
DevHiv3 <dev.hiv3e@gmail.com>
2025-04-20 01:31:19 +0530

    FIRST COMMIT

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,96 +1,56 @@
-import { Geist, Geist_Mono, Sixtyfour_Convergence, Inter_Tight, Orbitron, Sacramento, Rob  } from "next/font/google";
-import "../globals.css";
+"use client"
+import Image from "next/image";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import Tweets from "@/components/tweets";
 
-export const metadata = {
-  title: "Portfolio",
-  description: "Portfolio",
-  keywords: "resume, portfolio, expo, react, reactjs, reactnative,react native, nodejs, node js",
-  authors: [{ name: "DevHiv3" }],
-  openGraph: {
-    title: "Portfolio",
-    description: "Portfolio",
-    url: "https://tinyurl.com/instagram-clone-expo",
-    type: "website",
-    images: [
-      {
-        url: "https://blue-parental-mackerel-801.mypinata.cloud/ipfs/bafybeidvpeaznv4pcszj2zljhhmnwyridf2lgul73h3yt674u22eskixbm",
-        width: 1200,
-        height: 630,
-        alt: "logo",
-      },
-    ],
-  },
-  twitter: {
-    card: "https://blue-parental-mackerel-801.mypinata.cloud/ipfs/bafybeidvpeaznv4pcszj2zljhhmnwyridf2lgul73h3yt674u22eskixbm",
-    title: "Portfolio",
-    description: "Stay connected with real-time chat and social features.",
-    images: ["https://blue-parental-mackerel-801.mypinata.cloud/ipfs/bafybeidvpeaznv4pcszj2zljhhmnwyridf2lgul73h3yt674u22eskixbm"],
-  },
-  icons: {
-    icon: "https://blue-parental-mackerel-801.mypinata.cloud/ipfs/bafybeidvpeaznv4pcszj2zljhhmnwyridf2lgul73h3yt674u22eskixbm",
-    apple: "https://blue-parental-mackerel-801.mypinata.cloud/ipfs/bafybeidvpeaznv4pcszj2zljhhmnwyridf2lgul73h3yt674u22eskixbm",
-  },
-};
-
-const orbitron = Orbitron({
-  subsets: ['latin'],
-  variable: '--font-orbitron',  
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const interTight = Inter_Tight({
-  subsets: ["latin"], 
-});
-
-const sixtyfourConvergence = Sixtyfour_Convergence({
-  subsets: ["latin"],
-  variable: "--font-sixtyfour-convergence",
-});
-
-const sacramento = Sacramento({
-  weight: '400', // Regular weight for Sacramento
-  subsets: ['latin'],
-  variable: '--font-sacramento', // Custom CSS variable
-});
-
-export default function RootLayout({ children }) {
+export default function Home() {
   return (
-    <html lang="en">
-      <head>
-        {/* Add Google Fonts link in the <head> */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Orbitron:wght@400..900&family=Sacramento&family=Sixtyfour+Convergence&display=swap" rel="stylesheet"></link>
-        <link
-          href="https://cdn.jsdelivr.net/npm/@fontsource/fredoka-one/index.css"
-          rel="stylesheet"
-        />
-        
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <div className="relative flex flex-col items-center justify-center w-full bg-black">
+        <Navbar />
+   
+
+    {/* BANNER */}
+
+    <div className={`flex flex-col w-full h-4/5 justify-center items-center pt-96 sm:pt-72 `}> 
+        <h1 className="text-9xl text-white text-center uppercase  font-extrabold"> 99x</h1>
+        <span className="text-6xl text-white text-center uppercase  font-light"> Insights</span>
+
+    </div>
+
+        <div className="mt-96 sm:mt-40 w-full h-2 border-t border-white"></div>
+        <div className="mt-14 w-full h-2 border-t border-white"></div>
+        <div className="mt-14 w-full h-2 border-t border-white"></div>
+        <div className="mt-14 w-full h-2 border-t border-white"></div>
+        <div className="mt-14 w-full h-2 border-t border-white"></div>
+
+    {/* THE PROBLEM */}
+
+    <div className={`flex flex-col w-full  h-screen justify-start items-center mt-24`}> 
+
+        <div className="flex flex-col justify-between space-y-24 w-full mt-40 ">
+            <h1 className="text-4xl sm:text-6xl text-left ml-6 sm:ml-20 text-white uppercase montserrat-extrabold "> the problem ?</h1>
+            <p className="text-2xl sm:text-4xl text-left self-end mr-0 sm:mr-20 text-white uppercase montserrat-light w-80 sm:w-96">In today's era of AI and ML, brushing up skills have really become difficult</p>
+        </div>
+
+    </div>
+
+    {/* THE SOLUTION */}
+
+
+    <div className={`flex flex-col w-full h-screen justify-start items-center border-t border-white`}> 
+
+        <div className="flex flex-col justify-between space-y-12 w-full mt-40 ">
+            <h1 className="text-4xl sm:text-6xl text-left ml-6 sm:ml-20 text-white uppercase montserrat-extrabold "> the solution </h1>
+            <p className="text-2xl sm:text-4xl text-left self-end mr-0 sm:mr-20 text-white uppercase montserrat-light w-80 sm:w-96">In today's era of AI and ML, brushing up skills have really become difficult</p>
+        </div>
+
+    </div>
+
+    {/* */}
+
+    <Footer />
+
+    </div>
   );
 }
